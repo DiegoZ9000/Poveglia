9ad52ed3c3eea8434540eb2bc80ac169f6bcffdd
DZ9000 <diegozaccara@hotmail.com>
2019-12-06 15:59:36 -0300

    poveglia, quinto commit

--- web/views.py
+++ web/views.py
@@ -4,11 +4,11 @@ from django.shortcuts import render_to_response
 def index (request):
     return render_to_response('index.html')
 
-def index (request):
+def bio (request):
     return render_to_response('bio.html')
 
-def index (request):
+def links (request):
     return render_to_response('links.html')
 
-def index (request):
+def news (request):
     return render_to_response('news.html')
